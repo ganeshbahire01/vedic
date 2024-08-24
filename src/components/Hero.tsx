733b31ec6935d672d849857b006f1ac972bbe161
AbhiKandle <abhishekkandle4374@gmail.com>
2024-08-25 02:49:33 +0530

    conatiner adding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,18 +5,16 @@ type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div>
-      <div
-        className="w-full h-screen object-cover bg-no-repeat"
-        style={{ backgroundImage: "url('/hero.webp')" }}
-      >
-        <div className="">
-          <h1>we Provide Effective</h1>
-          <p>Ayurvedic Treatments</p>
-          <button className="text-white bg-[#93c942] py-2 px-3 rounded-lg">
-            Book Appointment
-          </button>
-        </div>
+    <div
+      className=" h-screen object-cover bg-repeat-round"
+      style={{ backgroundImage: "url('/hero.webp')" }}
+    >
+      <div className="">
+        <h1>we Provide Effective</h1>
+        <p>Ayurvedic Treatments</p>
+        <button className="text-white bg-[#93c942] py-2 px-3 rounded-lg">
+          Book Appointment
+        </button>
       </div>
     </div>
   );
