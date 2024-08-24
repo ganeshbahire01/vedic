8249508a99973e6708de39720f2f2614458bfb0a
Ganesh <ganesh@bigcircle.tech>
2024-08-25 03:28:23 +0530

    hero added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,11 +7,11 @@ type Props = {};
 const Hero = (props: Props) => {
   return (
     <div
-      className=" h-[580px] object-cover bg-repeat-round relative  "
+      className="h-[400px] md:h-[580px] object-cover bg-repeat-round relative  "
       style={{ backgroundImage: "url('/hero.webp')" }}
     >
-      <div className="absolute top-1/4 left-32 max-w-lg">
-        <h1 className="text-2xl leading-[44px] tracking-[-0.02em] font-bold lg:text-5xl mt-4 font-work-sans">
+      <div className="absolute top-10 left-5 md:top-1/4 md:left-32 max-w-lg ">
+        <h1 className="text-3xl leading-[44px] tracking-[-0.02em] font-bold md:text-5xl mt-4 font-work-sans">
           Shree Vishwastha Ayurvedalaya and Panchakarma Centre
         </h1>
         <p className="text-xl leading-7 font-normal mt-4">
